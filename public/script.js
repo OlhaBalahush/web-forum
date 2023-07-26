3cec19312532c78a63d8a714d12ab0e825af766f
Olya <balagush.olga01@gmail.com>
2023-07-26 17:04:15 +0300

    js file was added

---
+++ public/script.js
@@ -0,0 +1,5 @@
+var loginPopup = document.getElementById('log-in-popup')
+loginPopup.style.display = 'none'
+
+var signupPopup = document.getElementById('sign-up-popup')
+signupPopup.style.display = 'none'
